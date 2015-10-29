5c18371fd03d46c86bd7d3752022f4e250d4e31f
golub71 <golub71@gmail.com>
2015-10-29 14:30:18 -0400

    Fixed NewCustomerServlet

--- src/java/TOBA/Controllers/NewCustomerServlet.java
+++ src/java/TOBA/Controllers/NewCustomerServlet.java
@@ -3,10 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package TOBA.New_customer;
+package TOBA.Controllers;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -16,7 +17,7 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author robertstepanov
  */
-public class New_customerServlet extends HttpServlet {
+public class NewCustomerServlet extends HttpServlet {
 
     
     
@@ -24,8 +25,6 @@ public class New_customerServlet extends HttpServlet {
             throws ServletException, IOException {
         
         
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
            
             String firstName = request.getParameter("firstName");
             String lastName = request.getParameter("lastName");
@@ -48,10 +47,50 @@ public class New_customerServlet extends HttpServlet {
                 message = "";
                 url = "/Success.html";
             }
-            }
             
+            RequestDispatcher rd=request.getRequestDispatcher(url);
+                rd.forward(request, response);
         }
         
+     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
     }
 
   
